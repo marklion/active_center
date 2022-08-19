5746e450c978c7064c4b8ae425eec1c07001067d
Dong Chen <dongchen0502@163.com>
2022-08-20 02:34:06 +0800

    remove useless code

--- fe_pc/src/views/registration/stat.vue
+++ fe_pc/src/views/registration/stat.vue
@@ -29,31 +29,7 @@
           </el-table-column>
         </el-table>
       </el-card>
-
-
     </el-row>
-
-
-    <el-dialog title="请选择环号" :visible.sync="toySelectVisible" width="90%" @close="onCancelToySelect">
-      <el-form ref="form" :model="form" :rules="rules" label-width="80px">
-        <el-form-item label="选择数量">
-          <el-input v-model="editingItem.toy_limit" disabled></el-input>
-        </el-form-item>
-        <el-form-item label="候选环号" prop="checkedToys">
-          <el-checkbox-group
-            v-model="form.checkedToys"
-            :min="1"
-            :max="editingItem && editingItem.toy_limit">
-            <el-checkbox v-for="toy in toyList" :label="toy._id" :key="toy._id">{{toy.ring_no}}</el-checkbox>
-          </el-checkbox-group>
-        </el-form-item>
-      </el-form>
-      <div slot="footer" class="dialog-footer">
-        <el-button @click="onCancelToySelect">取 消</el-button>
-        <el-button @click="onResetForm">重 置</el-button>
-        <el-button type="primary" @click="onSaveBetRecord">确 定</el-button>
-      </div>
-    </el-dialog>
   </div>
 </template>
 
@@ -78,29 +54,6 @@ export default {
       activeItemPlayersMap: {},
 
       toyList : [],
-      toySelectVisible: false,
-      editingItem: {toy_limit : 0},
-
-      form : {
-        checkedToys : []
-      },
-      rules : {
-        checkedToys : [
-          {type: 'array', required: true, message: '请选择对应数量的环号', trigger: 'change'},
-          {type: 'array', required: true, validator: this.toysValidator, trigger: 'change'},
-        ]
-      },
-      toysValidator : function(){
-        let _this = this;
-        return function(rule, value, cb){
-          console.log(123123)
-            if(value.length === _this.editingItem.toy_limit){
-              cb();
-            }else{
-              return cb(new Error('请正确选择环号数量' + _this.editingItem.toy_limit))
-            }
-        }
-      }.bind(this)()
     }
   },
   beforeRouteEnter(to, from, next) {
@@ -131,58 +84,9 @@ export default {
     getTitleDisplay(){
       return this.active ? this.active.name : ''
     },
-    getItemSum(itemName){
-      let items = this.activeItemMap[itemName];
-      let result = _.reduce(items, (sum, item) => {
-        let records = this.activeItemPlayersMap[item._id];
-        return sum + (records ? records.length * item.bet_value : 0);
-      }, 0)
-      return result
-    },
-    getActiveSum(){
-      let sum = _.reduce(this.activeItems, (sum, itemName) => {
-        return sum + this.getItemSum(itemName)
-      }, 0)
-      return '明细：￥' + sum
-    },
+
     async loadToys(){
       this.toyList = await getToyList();
-    },
-    onResetForm(){
-      this.form.checkedToys = [];
-    },
-    onCancelToySelect(){
-      this.editingItem = {toy_limit : 0};
-      this.form.checkedToys = [];
-      this.toySelectVisible = false;
-    },
-    onSaveBetRecord(){
-      const loading = this.$loading({
-        lock: true,
-        text: '数据加载中',
-        spinner: 'el-icon-loading',
-        background: 'rgba(0, 0, 0, 0.7)'
-      });
-
-      this.$refs.form.validate(async valid => {
-        try{
-          if(valid){
-            let postData = {
-              toys: this.form.checkedToys,
-              item: this.editingItem._id
-            }
-            await saveActivePlayer(postData)
-            await this.reloadActivePlayers()
-            this.onCancelToySelect();
-          }else{
-            return false;
-          }
-        }catch(err){
-          console.log(err)
-        }finally{
-          loading.close()
-        }
-      })
     },
     async reloadActivePlayers(){
       this.activeItemPlayersMap = {}
@@ -196,27 +100,9 @@ export default {
         this.activeItemPlayersMap[key].push(group)
       }
       await this.$nextTick()
-      // this.activeItemPlayersMap = _.groupBy(this.activeItemPlayers, 'item');
     },
     getGroupDisplay(group){
       return _.map(group, 'toy.ring_no').join(' - ')
-    },
-    async onRemoveRecord(item, toyGroup){
-      try{
-        await this.$confirm('此操作将删除该报名记录, 是否继续?', '提示', {
-          confirmButtonText: '确定',
-          cancelButtonText: '取消',
-          type: 'warning'
-        })
-        for(let record of toyGroup){
-          await removePlayer(record._id);
-        }
-        await this.reloadActivePlayers();
-        this.$message({
-          type: 'success',
-          message: '删除成功!'
-        })
-      }catch(err){}
     },
     onFilterChange(filters){
       console.log('fileters = ', filters)
